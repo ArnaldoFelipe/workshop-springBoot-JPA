deefdbe3dfd9a6e2b566ac5bee2169be0f90bc03
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-09-21 00:13:59 -0300

    criando a associaçao de muitos para muitos com meu product e category

--- src/main/java/com/project/entities/Product.java
+++ src/main/java/com/project/entities/Product.java
@@ -4,11 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+
 
 @Entity
 @Table(name = "tb_product")
@@ -22,7 +26,10 @@ public class Product {
     private Double price;
     private String imgUrl;
 
-    @Transient
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_product_category", 
+    joinColumns = @JoinColumn(name ="product_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product(){}
